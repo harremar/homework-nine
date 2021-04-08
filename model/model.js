184e14b3082e73cd9dbb700a86cdaa1e8a8bf95b
harremar <78804575+harremar@users.noreply.github.com>
2021-04-07 20:22:54 -0400

    Initial commit

---
+++ model/model.js
@@ -0,0 +1,283 @@
+var homeContent = `<div class="home">
+<div class="header">
+    <h3>- SINCE 1987 -</h3>
+    <h1>ROOSTER ACRES</h1>
+    <h3> Chickens / Cows / Sheep</h3>
+</div>
+<div class="home-content">
+    <div class="about-section">
+        <div class="box">
+            <a id="about" href="#">About the Farm</a>
+        </div>
+    </div>
+    <div class="products-section">
+        <h1>FRESH FARM PRODUCTS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eius ad nostrum officia porro, vitae est temporibus animi doloribus velit sint modi quidem reiciendis, explicabo cumque dolore accusantium quaerat eaque!</p>
+        <div class="button">
+            <a id="products" href="#">Order Today</a>
+        </div>
+    </div>
+    <div class="contact-section">
+        <div class="box">
+            <a id="contact" href="#">Come Visit Us</a>
+        </div>
+    </div>
+</div>
+</div>
+<div class="quote-holder">
+<h1>WHAT OUR CLIENTS SAY</h1>
+<div class="quote">
+    <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore vero voluptas ullam cumque tempora error corrupti fugiat sed sit modi voluptatum recusandae." </p>
+    <h4>- Jamie Evans</h4>
+</div>
+<div class="quote">
+    <p>"Eius veritatis quod porro sunt vel dolor laboriosam qui deleniti omnis odit, nam fuga deserunt non doloribus possimus iusto explicabo, in delectus cupiditate numquam corporis eaque aperiam quas debitis. Vero."</p>
+    <h4>- Chris Jacobs</h4>
+</div>
+
+<div class="quote">
+    <p>"Totam architecto quasi quam atque ipsa repudiandae, nam culpa optio nisi eveniet sunt animi fuga numquam voluptate rem, labore aspernatur. At id voluptatum reprehenderit, velit consequuntur quis saepe fugit ea."</p>
+    <h4>- Amy Stevens</h4>
+</div>
+</div>
+
+<footer>
+<div class="footer-content">
+    <div class="logo"></div>
+    <div class="detail">
+        <h2>Deatils</h2>
+        <p>123 Street Name</p>
+        <p>Indianapolis, IN 46142</p>
+        <p>123-456-7890</p>
+        <p>info@myemail.com</p>
+    </div>
+    <div class="info">
+        <div class="social">
+            <div class="facebook"></div>
+            <div class="instagram"></div>
+            <div class="twitter"></div>
+        </div>
+        <p>copywrite 2021 by Marielle Harrell.</p>
+    </div>
+    
+</div>
+
+</footer>`
+var aboutContent = `<div class="about">
+<div class="ourstory-content">
+    <div class="ourstory-pic"></div>
+    <div class="ourstory-text">
+        <h1>This is Our Story</h1>
+        <h4>2nd Generation Family Owned Business</h4>
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dicta doloremque unde minima incidunt reprehenderit modi odio quo! Sunt earum amet fuga rem veritatis, pariatur vero sint commodi temporibus magnam?</span><span>Porro, cupiditate? Delectus iusto quibusdam repudiandae alias? Nesciunt, est accusantium doloribus autem voluptatem odio eaque numquam optio nemo doloremque. Dolor deserunt ut quia porro illum similique quae iste nam vero?</span><span>Dicta doloremque in minus repellat soluta odit inventore quo aliquid vitae dolores impedit sint sapiente, qui, voluptas temporibus facilis esse commodi a incidunt labore expedita aspernatur rem, dolor dolorem? Consequatur.</span></p>
+    </div>
+</div>
+<div class="aboutfarm">
+    <h1>More About Rooster Acres</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aliquid nemo itaque quis beatae, nisi magni! Soluta perferendis dolorem architecto ex nemo voluptatibus, voluptatum dignissimos non perspiciatis, laboriosam incidunt molestias quidem consectetur temporibus eius, numquam hic? Provident blanditiis maxime voluptate beatae itaque eligendi accusamus, nobis pariatur quasi omnis deleniti illo aut cumque quos iste voluptates neque cupiditate. Nostrum, quos facere. Asperiores possimus harum necessitatibus quas quos voluptates, enim magnam, perferendis, sed quisquam impedit ut ea odio!</p>
+    <p>Enim autem voluptas porro deserunt voluptatum reprehenderit odio eaque molestiae beatae magni neque odit commodi numquam delectus iure placeat fugiat tempora, adipisci voluptate. Aliquid sapiente eum pariatur! Reiciendis dicta atque omnis beatae voluptatibus molestiae voluptates. Iusto nisi earum impedit repellat adipisci? Nisi laudantium cupiditate libero quidem sapiente nobis sequi fugiat officia.</p>
+
+    <div class="button">
+        <a id="contact" href="#">Visit Us</a>
+    </div>
+    
+</div>
+<div class="wedo-content">
+    <div class="wedo-text">
+        <h1>WHAT WE DO</h1>
+        <h4>Raise Cattle / Produce Eggs / Etc.</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolore officiis doloremque quam provident dolorem optio dolor. Repellendus ipsa aperiam, veritatis a quam illo, voluptas quae laudantium eveniet obcaecati molestiae nulla voluptatem? Molestias, minima repellendus voluptates nihil quos vitae blanditiis cupiditate dolorum pariatur quasi voluptatem neque alias temporibus necessitatibus beatae? Odio maxime ea, dolores nobis fugiat eum unde! Ipsam aut, quasi repellendus nam dolores aspernatur? Expedita autem error laudantium dignissimos?
+        </p>
+    </div>
+    <div class="wedo-pic"></div>
+
+</div>
+</div>
+
+<footer>
+<div class="footer-content">
+    <div class="logo"></div>
+    <div class="detail">
+        <h2>Deatils</h2>
+        <p>123 Street Name</p>
+        <p>Indianapolis, IN 46142</p>
+        <p>123-456-7890</p>
+        <p>info@myemail.com</p>
+    </div>
+    <div class="info">
+        <div class="social">
+            <div class="facebook"></div>
+            <div class="instagram"></div>
+            <div class="twitter"></div>
+        </div>
+        <p>copywrite 2021 by Marielle Harrell.</p>
+    </div>
+</div>
+</footer>`;
+var productsContent = `<div class="products">
+<h1>SHOP PRODUCTS</h1>
+<div class="image-holder">
+<div class="item">
+    <div class="chick"></div>
+    <h2>Baby Chick</h2>
+    <p>$9.99</p>
+</div>
+
+<div class="item">
+    <div class="chicken"></div>
+    <h2>Chicken</h2>
+    <p>$24.99</p>
+</div>
+
+<div class="item">
+    <div class="rooster"></div>
+    <h2>Rooster</h2>
+    <p>$29.99</p>
+</div>
+</div>
+
+<div class="image-holder">
+<div class="item">
+    <div class="cocomilk"></div>
+    <h2>1 Gallon of Chocolate Milk</h2>
+    <p>$3.69</p>
+</div>
+<div class="item">
+    <div class="milk"></div>
+    <h2>1 Gallon of Organic Milk</h2>
+    <p>$3.69</p>
+</div>
+<div class="item">
+    <div class="icecream"></div>
+    <h2>Pint of Organic Chocolate Ice Cream</h2>
+    <p>$5.29</p>
+</div>
+</div>
+<div class="image-holder">
+<div class="item">
+    <div class="icecreamv"></div>
+    <h2>Pint of Organic Vanilla Ice Cream</h2>
+    <p>$5.09</p>
+</div>
+<div class="item">
+    <div class="icecreamc"></div>
+    <h2>Pint of Organic Carmel Ice Cream</h2>
+    <p>$5.29</p>
+</div>
+<div class="item">
+    <div class="icecreams"></div>
+    <h2>Pint of Organic Strawberry Ice Cream</h2>
+    <p>$5.45</p>
+</div>
+</div>
+<div class="image-holder2">
+<div class="item">
+    <div class="eggs"></div>
+    <h2>3 Organic Eggs</h2>
+    <p>$5.09</p>
+</div>
+<div class="item">
+    <div class="eggs6"></div>
+    <h2>6 Organic Eggs</h2>
+    <p>$9.49</p>
+</div>
+</div>
+</div>
+<footer>
+<div class="footer-content">
+<div class="logo"></div>
+<div class="detail">
+    <h2>Deatils</h2>
+    <p>123 Street Name</p>
+    <p>Indianapolis, IN 46142</p>
+    <p>123-456-7890</p>
+    <p>info@myemail.com</p>
+</div>
+<div class="info">
+    <div class="social">
+        <div class="facebook"></div>
+        <div class="instagram"></div>
+        <div class="twitter"></div>
+    </div>
+    <p>copywrite 2021 by Marielle Harrell.</p>
+</div>
+
+</div>
+
+</footer>`; 
+var contactContent = ` <div class="contact">
+<h1> Contact Us</h1>
+<div class="message">
+    <div class="border">
+        <h2>123-456-7890</h2>
+    </div>
+    <div class="border2">
+        <h2>info@myemail.com</h2>
+    </div>
+</div>
+
+<div class = "name-phone">
+    <div class="name">
+        <p>Name *</p>
+        <input type="text" size="5" maxlength="40" placeholder=" " >
+    </div>
+    <div class="phone">
+        <p>Phone</p>
+        <input type="text" size="5" maxlength="40" placeholder=" " >
+    </div>
+</div>
+
+<div class="email">
+    <p>Email *</p>
+    <input type="text" size="5" maxlength="40" placeholder="" >
+</div>
+
+<div class="entry">
+    <p>Message</p>
+    <input type="text" size="5" maxlength="40" placeholder="" >
+</div>
+
+    <div class="button">
+        <a href=" ">
+            SUBMIT
+        </a>
+    </div>
+
+
+</div>
+<footer>
+<div class="footer-content">
+    <div class="logo"></div>
+    <div class="detail">
+        <h2>Deatils</h2>
+        <p>123 Street Name</p>
+        <p>Indianapolis, IN 46142</p>
+        <p>123-456-7890</p>
+        <p>info@myemail.com</p>
+    </div>
+    <div class="info">
+        <div class="social">
+            <div class="facebook"></div>
+            <div class="instagram"></div>
+            <div class="twitter"></div>
+        </div>
+        <p>copywrite 2021 by Marielle Harrell.</p>
+    </div>
+    
+</div>
+
+</footer>`; 
+
+export function getPageContent(pageId) {
+$("#app").html(eval(pageId));
+}
+
+
+
+export function getName(){
+    console.log("Marielle");
+}
+
+export function getLastName() {
+    console.log("Harrell");
+}
